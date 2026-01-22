cec2140767df70416050819b9189a24c58bce8ac
fikz31 <fikri311002@gmail.com>
2026-01-22 12:02:29 +0800

    Delete Route directory

--- Route/apiRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { verifyTokenRoute } from "../Middleware/verifyToken.js";
-
-const router = express.Router();
-
-router.get("/verify-token", verifyTokenRoute);
-
-export default router;
